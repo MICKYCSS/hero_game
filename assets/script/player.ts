a0f13ed2faee58ac17608f8203ac549364436b18
chenshasha <chenshasha@chenshashadeMacBook-Air.local>
2023-05-17 20:45:02 +0800

    feat:左右跑动

--- assets/script/player.ts
+++ assets/script/player.ts
@@ -1,23 +1,28 @@
 import { _decorator, Component, Node,systemEvent,SystemEvent,EventKeyboard,KeyCode,RigidBody2D,
-    v3 } from 'cc';
+    v3,Animation } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('player')
 export class player extends Component {
-    private _rigidBody: RigidBody2D = null;
+    animationName:string = 'stand'
+    animation:Animation = null
     accLeft:boolean = false;
     accRight:boolean = false;
     xSpeed:number = 0;
     maxSpeed:number = 500
 
     update(deltaTime: number) {
+    let animationName = this.animationName
     // 每帧更新速度
      if(this.accLeft){
         this.xSpeed = -300;
+        animationName = 'run'
      }else if(this.accRight){
         this.xSpeed = 300;
+        animationName = 'run'
      }else{
         this.xSpeed = 0
+        animationName = 'stand'
      }
      if(Math.abs(this.xSpeed)>this.maxSpeed){
         this.xSpeed = this.maxSpeed*this.xSpeed/Math.abs(this.xSpeed);
@@ -26,6 +31,7 @@ export class player extends Component {
      const pos = this.node.getPosition()
      pos.x += this.xSpeed*deltaTime
      this.node.setPosition(pos)
+     this.setAnimation(animationName)
     }
 
     onLoad(){
@@ -34,11 +40,18 @@ export class player extends Component {
         this.accRight = false
         this.xSpeed = 0
 
-        this._rigidBody = this.node.getComponent(RigidBody2D)
+        // 获得动画对象
+        this.animation = this.node.getComponent(Animation)
+
 
         //初始化键盘监听
         systemEvent.on(SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
         systemEvent.on(SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    }
+
+    onDestroy(){
+        systemEvent.off(SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        systemEvent.off(SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
     }
 
     onKeyDown(event: EventKeyboard) {
@@ -49,10 +62,12 @@ export class player extends Component {
             case KeyCode.KEY_A:
             case KeyCode.ARROW_LEFT:
                 this.accLeft = true
+                this.node.setScale(v3(-1,1))
                 break;
             case KeyCode.KEY_D:
             case KeyCode.ARROW_RIGHT:
                 this.accRight = true
+                this.node.setScale(v3(1,1))
                 break;
             case KeyCode.SPACE:
                 break
@@ -65,12 +80,15 @@ export class player extends Component {
         this.accLeft = false
         this.accRight = false
         this.xSpeed = 0
-
     }
 
-    onDestroy(){
-        systemEvent.off(SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-        systemEvent.off(SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    // 切换动画
+    setAnimation(animationName:string){
+        if(this.animationName === animationName)return
+        this.animationName = animationName
+        this.animation.play(animationName)
     }
+
+  
 }
 
