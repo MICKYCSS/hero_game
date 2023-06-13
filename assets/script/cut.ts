3546a5a6e747c7eb36a67e25d7af68456e97b84a
chenshasha <chenshasha@chenshashadeMacBook-Air.local>
2023-06-13 20:53:52 +0800

    音乐文件

--- assets/script/cut.ts
+++ assets/script/cut.ts
@@ -1,4 +1,7 @@
-import { _decorator, Component, Node, Graphics, color,UITransform } from 'cc';
+import {
+    _decorator, Component, Node, Graphics, Color, Canvas, UITransform, systemEvent, SystemEvent, v3,
+    v2, Camera, director, CameraComponent
+} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('cut')
@@ -6,7 +9,7 @@ export class cut extends Component {
     private startPos = null // 划动开始位置
     private endPos = null // 划动结束位置
     private lineNode = null // 划动轨迹效果
-    private lineGraphics = null // 划动轨迹的绘制组件
+    private lineGraphics: Graphics = null // 划动轨迹的绘制组件
 
     onTouchStart(event) {
         console.log('onTouchStart')
@@ -17,34 +20,45 @@ export class cut extends Component {
     }
 
     onTouchMove(event) {
-        console.log('onTouchMove')
+        console.log('onTouchMove', event, event)
         let pos = event.getLocation() // 当前的触摸位置
-        let deltaX = pos.x - this.startPos.x // X轴位移
-        let deltaY = pos.y - this.startPos.y // Y轴位移
-        if (deltaY < -50) { // 处理向上划动时不生成轨迹效果
-            if (this.lineGraphics) {
-                this.lineGraphics.clear()
-                this.lineNode.removeFromParent()
-            }
-            return
-        }
-        if (deltaX > 50 || deltaX < -50) { // 处理左右位移过大时不生成轨迹效果
-            if (this.lineGraphics) {
-                this.lineGraphics.clear()
-                this.lineNode.removeFromParent()
-            }
-            return
-        }
+        // let deltaX = pos.x - this.startPos.x // X轴位移
+        // let deltaY = pos.y - this.startPos.y // Y轴位移
+        // if (deltaY < -100) { // 处理向上划动时不生成轨迹效果
+        //     if (this.lineGraphics) {
+        //         this.lineGraphics.clear()
+        //         this.lineNode.removeFromParent()
+        //     }
+        //     return
+        // }
+        // if (deltaX > 200 || deltaX < -200) { // 处理左右位移过大时不生成轨迹效果
+        //     if (this.lineGraphics) {
+        //         this.lineGraphics.clear()
+        //         this.lineNode.removeFromParent()
+        //     }
+        //     return
+        // }
         if (!this.lineGraphics) { // 如果绘制组件为空，创建一个
-            this.lineGraphics = this.lineNode.addComponent(Graphics)
+            this.lineGraphics = this.node.parent.addComponent(Graphics)
         }
         this.lineGraphics.lineWidth = 10 // 设置轨迹的宽度
-        this.lineGraphics.strokeColor = color(0, 0, 0, 50) // 设置轨迹颜色以及不透明度
-        const uiTransform = this.node.getComponent(UITransform)
-        let startPoint = uiTransform.convertToNodeSpaceAR(this.startPos) // 节点空间转化
-        let endPoint = uiTransform.convertToNodeSpaceAR(pos) // 节点空间转化
-        this.lineGraphics.moveTo(startPoint.x, startPoint.y) // 移动到起始点
-        this.lineGraphics.lineTo(endPoint.x, endPoint.y) // 划动到结束点
+        this.lineGraphics.strokeColor = Color.RED; // 设置轨迹颜色以及不透明度
+        const uiTransform = this.lineNode.getComponent(UITransform)
+        let startPoint = uiTransform.convertToNodeSpaceAR(v3(this.startPos.x, this.startPos.y, 0)) // 节点空间转化
+        let endPoint = uiTransform.convertToNodeSpaceAR(v3(pos.x, pos.y, 0)) // 节点空间转化
+        const getPos = (point) => {
+            const { x, y } = this.node.parent.getPosition()
+            return [
+                (point.x - x) / 2,
+                (point.y - y) / 2,
+            ]
+
+        }
+        const [startX, startY] = getPos(startPoint)
+        const [endX, endY] = getPos(endPoint)
+        this.lineGraphics.moveTo(startX, startY) // 移动到起始点
+        this.lineGraphics.lineTo(endX, endY) // 划动到结束点
+        this.lineGraphics.stroke();
         this.startPos = pos
     }
 
@@ -53,6 +67,7 @@ export class cut extends Component {
         this.endPos = event.getLocation() // 划动结束位置
         let lineHeight = this.endPos.y - this.startPos.y
         let lineWidth = this.endPos.x - this.startPos.x
+        this.lineGraphics.clear()
         if (!this.lineNode) {
             return
         }
@@ -79,10 +94,9 @@ export class cut extends Component {
         // }
     }
     onLoad() {
-        console.log('cut')
-        this.node.on(Node.EventType.TOUCH_START, ()=>{console.log('TOUCH_START')}, this)
-        this.node.on(Node.EventType.TOUCH_MOVE, this.onTouchMove, this)
-        this.node.on(Node.EventType.TOUCH_END, this.onTouchEnd, this)
+        systemEvent.on(SystemEvent.EventType.TOUCH_START, this.onTouchStart, this)
+        systemEvent.on(SystemEvent.EventType.TOUCH_MOVE, this.onTouchMove, this)
+        systemEvent.on(SystemEvent.EventType.TOUCH_END, this.onTouchEnd, this)
 
     }
 
