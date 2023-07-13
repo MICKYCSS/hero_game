d0083f1ec4ff7ccf3361b2746ce43832caa4ca04
chenshasha <chenshasha@chenshashadeMacBook-Air.local>
2023-07-14 01:38:57 +0800

    feat:完善

--- assets/script/game.ts
+++ assets/script/game.ts
@@ -3,8 +3,8 @@ const { ccclass, property } = _decorator;
 
 @ccclass('game')
 export class game extends Component {
-    private player1 = { step: 0, node: null }
-    private player2 = { step: 0, node: null }
+    private player1 = { circle:0,step: 0, node: null, moneyNode: null, diamondNode: null, money: 1000, diamond: 0 }
+    private player2 = { circle:0,step: 0, node: null, moneyNode: null, diamondNode: null, money: 1000, diamond: 0 }
     private round: number = 1;// 1为player1的回合，2为player2的回合
     private positionX: number = 542.567;//右上角横坐标
     private positionY: number = 332.467;//右上角纵坐标
@@ -13,6 +13,7 @@ export class game extends Component {
     private label: Label = null
     private interval: number = 0.1 // 定时器的更新间隔时间，单位为秒
     private question: Node = null;
+    private showQuestion: boolean = false;
     private map = {
         supermarket: [6, 14, 25, 33] as number[],
         ground: [
@@ -37,24 +38,31 @@ export class game extends Component {
     }
 
     back() {
+        this.node.getChildByName('gameover').setScale(0,0)
         director.loadScene('scene')
     }
 
     update(deltaTime: number) {
     }
 
-    onQuestion(){
-        const label =  this.question.getComponent(Label)
+    onQuestion() {
+        const label = this.question.getComponent(Label)
         label.string = '是否购买钻石？花费200'
+        this.showQuestion = true
+    }
+    hideQuestion() {
+        this.showQuestion = false
+        const label = this.question.getComponent(Label)
+        label.string = ''
     }
 
 
     onMove(player, step) {
         const node = player.node
         const { x, y } = node.getPosition()
-        console.log(x, y, 'xx')
         if (player.step >= 38) {
             player.step = 0
+            player.circle = 1
         }
         if (player.step > 33) {
             node.setPosition(this.positionX + (player.step - 11 - 33) * this.moveX, this.positionY)
@@ -72,54 +80,110 @@ export class game extends Component {
             node.setPosition(this.positionX, this.positionY - (player.step - 6) * this.moveY)
         }
 
-
     }
 
     onKeyDown(event: EventKeyboard) {
+        if(this.player1.circle&&this.player2.circle)return
         const step = +this.label.string
         const round = this.node.getChildByName('round').getComponent(Label)
-        if (this.round === 1 && event.keyCode === KeyCode.KEY_W) {
-            this.stopAction();
-            this.player1.step += step
-            this.onMove(this.player1, step)
-            if(this.map.supermarket.includes(this.player1.step)){
-                this.onQuestion()
-            }
+        const setMoney = (player: any) => {
+            player.money -= 200
+            player.diamond += 1
+            player.moneyNode.getComponent(Label).string = player.money
+            player.diamondNode.getComponent(Label).string = player.diamond
+            this.hideQuestion()
+        }
+        const turnPlayer1 = () => {
+            this.hideQuestion()
+            this.round = 1
+            setTimeout(() => {
+                this.schedule(this.updateTimer, this.interval)
+                round.string = '玩家1'
+            }, 1000)
+        }
+        const turnPlayer2 = () => {
+            this.hideQuestion()
             this.round = 2
             setTimeout(() => {
                 this.schedule(this.updateTimer, this.interval)
                 round.string = '玩家2'
             }, 1000)
         }
-        if (this.round === 2 && event.keyCode === KeyCode.ARROW_UP) {
-            this.stopAction();
-            this.player2.step += step
-            this.onMove(this.player2, step)
-            this.round = 1
-            round.string = '玩家1'
-            setTimeout(() => {
-                this.schedule(this.updateTimer, this.interval)
-            }, 1000)
+        if (this.round === 1) {
+            if (event.keyCode === KeyCode.KEY_W) {
+                if (!this.showQuestion) {
+                    this.stopAction();
+                    this.player1.step += step
+                    this.onMove(this.player1, step)
+                    if (this.map.supermarket.includes(this.player1.step)) {
+                        this.onQuestion()
+                        return
+                    }
+                    turnPlayer2()
+
+                } else {
+                    setMoney(this.player1)
+                    turnPlayer2()
+                }
+                return
+            }
+            if (event.keyCode === KeyCode.KEY_S) {
+                turnPlayer2()
+            }
+
+            return
+        }
+        if (this.round === 2) {
+            if (event.keyCode === KeyCode.ARROW_UP) {
+                if (!this.showQuestion) {
+                    this.stopAction();
+                    this.player2.step += step
+                    this.onMove(this.player2, step)
+                    if (this.map.supermarket.includes(this.player2.step)) {
+                        this.onQuestion()
+                        return
+                    }
+                    turnPlayer1()
+                } else {
+                    setMoney(this.player2)
+                    turnPlayer1()
+                }
+                return
+            }
+            if (event.keyCode === KeyCode.ARROW_DOWN) {
+                turnPlayer1()
+            }
         }
     }
 
+    gameOver(){
+        this.node.getChildByName('gameover').setScale(1,1)
+    }
+
     onLoad() {
-        const player1 = this.node.getChildByName('player1')
-        const player2 = this.node.getChildByName('player2')
         this.question = this.node.getChildByName('question')
-        this.player1.node = player1
-        this.player2.node = player2
+        this.player1.node = this.node.getChildByName('player1')
+        this.player2.node = this.node.getChildByName('player2')
+        this.player1.moneyNode = this.node.getChildByName('money1')
+        this.player1.diamondNode = this.node.getChildByName('diamond1')
+        this.player2.moneyNode = this.node.getChildByName('money2')
+        this.player2.diamondNode = this.node.getChildByName('diamond2')
         const label = this.node.addComponent(Label)
         label.string = ''
         this.label = label
+        
         // 启动定时器
         this.schedule(this.updateTimer, this.interval);
         systemEvent.on(SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
 
     }
     updateTimer(dt) {
-        const random = Math.ceil(Math.random() * 6)
+        // const random = Math.ceil(Math.random() * 6)
+        const random = 6
         this.label.string = `${random}`
+        if(this.player1.circle&&this.player2.circle){
+            this.gameOver()
+        }
     }
 
     stopAction() {
